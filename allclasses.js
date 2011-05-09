d5d50bcedf4af317acde63b33e48fe704c216440
Mario Mueller <mario.mueller.work@gmail.com>
2011-05-09 09:54:34 +0200

    Changed doc generator from doxygen to apigen

---
+++ allclasses.js
@@ -0,0 +1,2 @@
+
+var classes = ["Closure","InvalidArgumentException","OutOfRangeException","Prowl\\Connector","Prowl\\Message","Prowl\\Response","Prowl\\Security\\PassthroughFilterImpl","Prowl\\Security\\Secureable","RuntimeException"];
--- tests/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	'apikey' => 'e0bf09a4cc20ae0bcd63b30b19031ef59a458634',
-	'providerkey' => 'b13b6c4fa612923cf8da205e24f374e2bc54fbed',
-	'tokenurl'	=> 'http://localhost/unittest'
-);
