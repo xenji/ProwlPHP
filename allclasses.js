d94a7577ff9e3ca118b8518c0427babf305a2a24
Mario Mueller <mario.mueller@trivago.com>
2012-01-20 16:09:34 +0100

    Removed docs

--- allclasses.js
+++
@@ -1,2 +0,0 @@
-
-var classes = ["Closure","InvalidArgumentException","OutOfRangeException","Prowl\\Connector","Prowl\\Message","Prowl\\Response","Prowl\\Security\\PassthroughFilterImpl","Prowl\\Security\\Secureable","RuntimeException"];
